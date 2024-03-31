cc79693e2699ccdea1fd71da02b574c5661d3265
RoopeK <35376295+Toopemus@users.noreply.github.com>
2024-03-31 16:42:44 +0300

    documentation for Model and Transform

--- src/renderer/model.rs
+++ src/renderer/model.rs
@@ -3,10 +3,14 @@ use std::{error::Error, fs};
 
 use nalgebra::{Point3, Rotation3, Scale3, Translation3};
 
+/// Stores transformation data that is applied to vertices: position, scale, rotation.
 #[derive(Debug)]
 pub struct Transform {
+    /// Position in relation to the origin.
     pub position: Translation3<f32>,
+    /// Scaling factors for x, y, and z dimensions.
     pub scale: Scale3<f32>,
+    /// Rotation around the x, y, and z axis in radians.
     pub rotation: Rotation3<f32>,
 }
 
@@ -20,6 +24,10 @@ impl Transform {
     }
 }
 
+/// Struct that manages individual 3D objects.
+///
+/// Models are represented as index and vertex buffers. Stores also the transformation data of the
+/// object.
 #[derive(Debug)]
 pub struct Model {
     vertex_buffer: Vec<Vertex>,
@@ -28,7 +36,9 @@ pub struct Model {
 }
 
 impl Model {
-    /// Loads an .obj file, THIS IS MOSTLY UNFINISHED AND INCORRECT, LOADS SOME FILES OK
+    /// Loads and initializes a model from an .obj file wrapped in a Result.
+    ///
+    /// **This is mostly unfinished and incorrect, loads some files OK.**
     pub fn load_from_file(filename: &str) -> Result<Model, Box<dyn Error>> {
         let mut vertex_buffer: Vec<Vertex> = vec![];
         let mut index_buffer: Vec<Face> = vec![];
@@ -65,24 +75,29 @@ impl Model {
         })
     }
 
+    /// Getter for the model's index buffer.
     pub fn index_buffer(&self) -> &Vec<Face> {
         &self.index_buffer
     }
 
+    /// Applies model transform and returns the vertex at index.
     pub fn transform_and_get_vertex_at(&self, index: usize) -> Vertex {
         let transformed_vertex = self.vertex_buffer[index].get_transformed(&self.transform);
 
         transformed_vertex
     }
 
+    /// Translates the object in 3D space.
     pub fn translate(&mut self, x: f32, y: f32, z: f32) {
         self.transform.position = Translation3::new(x, y, z);
     }
 
+    /// Scales the object along the x, y, and z axis.
     pub fn scale(&mut self, x: f32, y: f32, z: f32) {
         self.transform.scale = Scale3::new(x, y, z);
     }
 
+    /// Rotates the object by x, y, and z degrees along the respective axis.
     pub fn rotate(&mut self, x: f32, y: f32, z: f32) {
         self.transform.rotation =
             Rotation3::from_euler_angles(x.to_radians(), y.to_radians(), z.to_radians());
